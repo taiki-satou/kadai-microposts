153b4f989136bae545acd09ba9f951090847645f
taiki.satou <satotaijj@gmail.com>
2020-05-11 13:13:28 +0000

    second commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,9 +36,7 @@ class User < ApplicationRecord
   end
   
   def favorite(other_micropost)
-    unless self == other_micropost
-      self.favorites.find_or_create_by(micropost_id: other_micropost.id)
-    end
+    self.favorites.find_or_create_by(micropost_id: other_micropost.id)
   end
   
   def unfavorite(other_micropost)
